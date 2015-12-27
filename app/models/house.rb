27b666265d8859877b760831af3c1ea0b2f189de
Eloy Espinaco <eloyesp@gmail.com>
2015-12-27 18:03:29 -0300

    Geocode houses.

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,11 @@
 class House < ActiveRecord::Base
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  validates :address, presence: true
+
+  def full_address
+    address + ", Santa Fe, Santa Fe, Argentina"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227203933) do
+ActiveRecord::Schema.define(version: 20151227205143) do
 
   create_table "houses", force: :cascade do |t|
     t.string   "address"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20151227203933) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
